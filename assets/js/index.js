a6ef712661f5b1be905f0daa9a5e105f0f9df2e0
egor2910 <147133922+egor2910@users.noreply.github.com>
2024-01-12 17:57:30 +1000

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+
+
+// alert(name1)
+// let age = prompt("give me your age",0)
+// console.log(85+96, '1'+1, 1+ +'2','1'+'3',3/'3' );
+let name = "roge"
+let age = "69"
+let boolean = true;
+let nuuul = null
+let underfert = undefined
+let peopel = (
+    name:"kto tam",
+    age:18,
+    lust:true
+    cjrildren:
+)
