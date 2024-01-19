191ff64d23e7a2ddcd85a9db0518d45e73628bb6
egor2910 <147133922+egor2910@users.noreply.github.com>
2024-01-19 17:56:08 +1000

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,29 @@
+let h1 = document.getElementsByTagName("h1")
 
+console.log(h1);
+let h2Mass = document.getElementsByTagName("h3")
+console.log(h2Mass);
+h2Mass[0].innerText = "новый заголовок"
+let classElem = document.getElementsByClassName
+("header-menu-item")
+classElem[classElem.length - 1].innerHTML = "fight"
 
-// alert(name1)
-// let age = prompt("give me your age",0)
-// console.log(85+96, '1'+1, 1+ +'2','1'+'3',3/'3' );
-let name = "roge"
-let age = "69"
-let boolean = true;
-let nuuul = null
-let underfert = undefined
-let peopel = (
-    name:"kto tam",
-    age:18,
-    lust:true
-    cjrildren:
-)
+let work = document.getElementById("work")
+let educatiom = document.getElementById("education")
+
+console.log(work, education)
+
+work.style.backgroundColor = "rgb(255,100,0)"
+console.log(work.style);
+work.onclick = function () {
+    education.style.display = "flex"
+}
+education.onclick = () => {    
+    console.log(this);
+}
+    
+    
+    a = document.querySelectorAll("header-menu-item")
+
+console.log(a);
+work.style
